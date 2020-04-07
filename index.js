96eba72aec44036bc1ab676da044c1ab77c14f59
Mohaimeen Hossain <mhossain5464@gmail.com>
2020-04-07 18:23:55 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,50 @@
+function shout(string){
+return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  var spy = string.toUpperCase()
+  console.log(spy)
+}
+
+function logWhisper(string){
+  var spy = string.toLowerCase()
+  console.log(spy)
+}
+
+function sayHiToGrandma(string){
+  var lowercase = 'hello'
+  string.toLowerCase() === lowercase
+  var result = "I can't hear you!"
+
+  var uppercase = 'HELLO'
+  string.toUpperCase() === uppercase
+  string = "YES INDEED!"
+
+  return result
+
+  // return string
+  // var mixedCase = 'I love you, Grandma.'
+  // mixedCase.toLowerCase() === mixedCase
+  // mixedCase.toUpperCase === mixedCase
+
+}
+  //   function whisper(string){
+  //   }
+  //   return "I can\'t hear you!";
+  //
+  //   function shout(string){
+  //   }
+  //   return "YES INDEED!";
+  // }
+
+
+  //
+  // var mixedCase = 'I love you, Grandma.'
+  // string.toLowerCase() === mixedCase
+  // string.toUpperCase() === mixedCase
+  // return 'I love you, too.'
